8d3ed8db6b09eef1e8042106b3c1a82e758ba25b
Rodrigo Ramírez <8rb.academic@gmail.com>
2020-07-16 20:03:49 -0500

    first commit

---
+++ src/components/SelectedTypes.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "SelectedTypes"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TypesTable.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "TypesTable"
+    }
+</script>
+
+<style scoped>
+
+</style>
